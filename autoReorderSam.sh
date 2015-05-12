89c682e079edf0c8f255303c4fff8d19b8f98fa3
d-cole <daniel.cole@mail.utoronto.ca>
2015-05-12 11:14:30 -0400

    modified to execute commands

--- autoReorderSam.sh
+++ autoReorderSam.sh
@@ -8,6 +8,6 @@ read OUTLOC
 for bamFile in $FILES*.bam
     do
     outName=${bamFile##*/}
-    echo "java -Xmx2g -jar /data/daniel.cole/tools/picard-tools-1.96/ReorderSam.jar INPUT=$bamFile OUTPUT=$OUTLOC${outName/.bam/_reorder.bam} &"
+    java -Xmx2g -jar /data/daniel.cole/tools/picard-tools-1.96/ReorderSam.jar INPUT=$bamFile OUTPUT=$OUTLOC${outName/.bam/_reorder.bam} &
 
 done
