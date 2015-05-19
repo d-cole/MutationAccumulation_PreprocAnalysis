76879563de3fe43f35468c94afbdd5e2767e65c1
d-cole <daniel.cole@mail.utoronto.ca>
2015-05-19 11:09:06 -0400

    processes one file at a time with higher memory usage

--- autoSortSam.sh
+++ autoSortSam.sh
@@ -2,13 +2,8 @@
 LOC=$1
 arr=($LOC*_readgroup.bam)
 size=${#arr[@]}
-for ((i=0; i<$size; i+=2)); do
-    i2=$((i+1))
-    java -Xmx2g -jar ~/tools/picard-tools-1.96/SortSam.jar INPUT=${arr[$i]} OUTPUT=${arr[$i]/.bam/_sorted.bam} SORT_ORDER=coordinate &
-    if [ $i2 -lt $size ]
-        then
-            java -Xmx2g -jar ~/tools/picard-tools-1.96/SortSam.jar INPUT=${arr[$i2]} OUTPUT=${arr[$i2]/.bam/_sorted.bam} SORT_ORDER=coordinate  
-    fi
+for ((i=0; i<$size; i+=1)); do
+    java -Xmx4g -jar ~/tools/picard-tools-1.96/SortSam.jar INPUT=${arr[$i]} OUTPUT=${arr[$i]/.bam/_sorted.bam} SORT_ORDER=coordinate
 
 done
 
