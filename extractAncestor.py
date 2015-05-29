e5c9203e4859e4ca5cd988b95ee251dfaaeb745a
Daniel <daniel.cole@mail.utoronto.ca>
2015-05-29 11:14:36 -0400

    fixed mixed tabs and spaces

--- extractAncestor.py
+++ extractAncestor.py
@@ -9,24 +9,24 @@ GP2_3_idx = 16
 
 
 def parseAnc(line):
-	"""
-	Constructs new line with only samples CC3-3 and GP2-3
-	"""
-	line_col = str.split(line)
+    """
+    Constructs new line with only samples CC3-3 and GP2-3
+    """
+    line_col = str.split(line)
     new_line = ""
     for i in range(0,9):
         new_line = new_line + line_col[i] + " "
-	return new_line + " " + line_col[11] + " " + line_col[16]
+    return new_line + " " + line_col[11] + " " + line_col[16]
 
 if __name__ == "__main__":
-	anc_file,out_name = sys.argv[1],sys.argv[2]
-	outFile = open(out_name,'w')
-	with open(anc_file) as f:
-		for line in f:
-			if spirodelaFiltering.isDataLine(line):
-				outFile.write(parseAnc(line))
-			else:
-				outFile.write(line)
+    anc_file,out_name = sys.argv[1],sys.argv[2]
+    outFile = open(out_name,'w')
+    with open(anc_file) as f:
+        for line in f:
+            if spirodelaFiltering.isDataLine(line):
+                outFile.write(parseAnc(line))
+            else:
+                outFile.write(line)
 
-	f.close()
-	outFile.close()
+    f.close()
+    outFile.close()
