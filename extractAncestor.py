c0f8f9c3c4a21e3bc64d45b0a783e7c47df4225c
d-cole <daniel.cole@mail.utoronto.ca>
2015-05-29 11:11:18 -0400

    Fixed new_line creation, tabs/spaces error needs fixing

--- extractAncestor.py
+++ extractAncestor.py
@@ -1,4 +1,5 @@
 import spirodelaFiltering
+import sys
 """
 Extracts CC3-3 and GP2-3 samples from vcf file
 """
@@ -12,9 +13,10 @@ def parseAnc(line):
 	Constructs new line with only samples CC3-3 and GP2-3
 	"""
 	line_col = str.split(line)
-	return line_col[0:9] + " " + line_col[11] + " " + line_col[16]
-
-
+    new_line = ""
+    for i in range(0,9):
+        new_line = new_line + line_col[i] + " "
+	return new_line + " " + line_col[11] + " " + line_col[16]
 
 if __name__ == "__main__":
 	anc_file,out_name = sys.argv[1],sys.argv[2]
