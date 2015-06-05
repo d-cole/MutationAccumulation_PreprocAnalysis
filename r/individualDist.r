e8cfba5e5c61dff230073be2c190568c04b02b43
Daniel <daniel.cole@mail.utoronto.ca>
2015-06-05 17:09:52 -0400

    new individual dist for GP2-3 and CC3-3 w/o CC3-3M

--- r/individualDist.r
+++ r/individualDist.r
@@ -1,6 +1,7 @@
-setwd("~/Documents/spirodela/data/CC3-3/individualData/allCases/")
-files <-list.files(path="~/Documents/spirodela/data/CC3-3/individualData/allCases/",
+setwd("~/Documents/spirodela/data/GP2-3/individualData/")
+files <-list.files(path="~/Documents/spirodela/data/GP2-3/individualData/",
                    pattern="*.csv",full.names=F,recursive=FALSE)
+
 remExt <- function(x,ext){
   return (sub(ext,"",x))
 }
@@ -11,7 +12,7 @@ for (i in 1:length(files)){
     names(l)[i]<-remExt(files[i],".csv")
 }
 do.call(cbind,l)
-write.table(l,file="ind_DP_med.csv",append=FALSE,sep=" ",
+write.table(l,file="ind_DP_med_v2.csv",append=FALSE,sep=" ",
             eol="\n",na = "NA",dec=".",row.names=FALSE,
             col.names=TRUE)
 
