458126d51f28e1a9468dc59a71acabee32f50f25
Daniel <daniel.cole@mail.utoronto.ca>
2015-06-11 13:12:01 -0400

    added chart for %mapped for all samples

--- r/plotAD.r
+++ r/plotAD.r
@@ -4,27 +4,81 @@
 #"varID","CHROM","POS","REF","ALT","QUAL","FILTER","AC",
 #"AF","AN","BaseQRankSum","siteDP","Dels","FS","HaplotypeScore",
 #"InbreedingCoeff","MLEAC","MLEAF","MQ","MQ0","MQRankSum","QD",
-#"ReadPosRankSum","odd_GT","odd_sample","cohort_GT","AD_alt",
+#"ReadPosRankSum","SOR","odd_GT","odd_sample","cohort_GT","AD_alt",
 #"AD_ref","AD_altSum","AD_refSum","odd_GQ","odd_PL","Anc_GT","Anc_sample"
 
 ## Collect arguments
 #Commandline arg info from http://www.r-bloggers.com/parse-arguments-of-an-r-script/
 args <- commandArgs(TRUE)
-## Default setting when no arguments passed
-
 if(length(args) < 1) {
-  cat("Please specify location of .csv file")
+  cat("Please specify location of .csv file \n")
 }
-all_samples<-read.csv(args,stringsAsFactors=FALSE,na.string=".")
-all_samples <- transform(all_samples,alt_ADvADSum = AD_alt/AD_altSum)
-print(head(all_samples))
 
-case_AD <- (all_samples[all_samples$odd_GT=='0/1' & all_samples$cohort_GT=='0/0',all_samples$alt_ADvADsum])
-str(case_AD)
+#Read in csv file
+all_samples<-read.csv(args,stringsAsFactors=FALSE,na.string=".")
+
+#Add column mAltvAltSum which is mutant alt reads divided by sum of alt reads at that site
+all_samples$mAltvAltSum <- all_samples$AD_alt/all_samples$AD_altSum
+
+
+#Plot mutant alternate reads / sum of alternate reads at site
 jpeg("CC3-3_f7_o0.1_c0.0_ADalt.ADaltSum.jpg")
-plot(hist((all_samples$AD_alt/all_samples$AD_altSum),prob=F,50),
+plot(hist((all_samples$mAltvAltSum),prob=F,50),
     main="CC3-3_f7 (Mutant alt reads) / (Sum of alt reads)",xlab="(Odd AD alt)/(AD alt Sum)")
 dev.off()
+
+jpeg("oddGQ-mAltvAltSum.jpg")
+with(all_samples,plot(odd_GQ ~ mAltvAltSum))
+dev.off()
+
+jpeg("Hscore-mAltvAltSum.jpg")
+with(all_samples,plot(HaplotypeScore ~ mAltvAltSum))
+dev.off()
+
+jpeg("MQ-mAltvAltSum.jpg")
+with(all_samples,plot(MQ ~ mAltvAltSum))
+dev.off()
+
+jpeg("MQ0-mAltvAltSum.jpg")
+with(all_samples,plot(MQ0 ~ mAltvAltSum))
+dev.off()
+
+jpeg("QD-mAltvAltSum.jpg")
+with(all_samples,plot(QD ~ mAltvAltSum))
+dev.off()
+
+jpeg("ReadPosRankSum-mAltvAltSum.jpg")
+with(all_samples,plot(ReadPosRankSum ~ mAltvAltSum))
+dev.off()
+
+jpeg("FS-mAltvAltSum.jpg")
+with(all_samples,plot(FS ~ mAltvAltSum))
+dev.off()
+
+jpeg("SOR-mAltvAltSum.jpg")
+with(all_samples,plot(SOR ~ mAltvAltSum))
+dev.off()
+
+jpeg("Dels-mAltvAltSum.jpg")
+with(all_samples,plot(Dels ~ mAltvAltSum))
+dev.off()
+
+jpeg("siteDP-mAltvAltSum.jpg")
+with(all_samples,plot(siteDP ~ mAltvAltSum))
+dev.off()
+
+jpeg("InbreedingCoeff-mAltvAltSum.jpg")
+with(all_samples,plot(InbreedingCoeff ~ mAltvAltSum))
+dev.off()
+
+jpeg("MLEAC-mAltvAltSum.jpg")
+with(all_samples,plot(MLEAC ~ mAltvAltSum))
+dev.off()
+
+jpeg("MLEAF-mAltvAltSum.jpg")
+with(all_samples,plot(MLEAF ~ mAltvAltSum))
+dev.off()
+
 
 
 
