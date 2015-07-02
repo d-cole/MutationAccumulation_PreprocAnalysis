385411e61659d4a6ab030fe4857a47853f991057
Daniel <daniel.cole@mail.utoronto.ca>
2015-07-02 14:01:27 -0400

    Added plots for commoon sites between samples G & M. G & M are contaminants, not sure which sample they are in GP2-3 or if G is even from GP2-3. Now continuing analysis excluding G from CC3-3.

--- postFilt/sharedSites.py
+++ postFilt/sharedSites.py
@@ -12,14 +12,17 @@ def isDataLine(line):
         return line[0] != "#"
     return False
 
-def getExclInt(samples,bSetIdx,aSet):
+def getExclInt(samples,bSetIdx,targetDict):
     """
     Returns the set of sites that A shares with B and only B
     """
     bSet = set(samples[bSetIdx].getKeys())
+    aSet = set(targetDict.getKeys())
     intAB = set.intersection(aSet,bSet)
+
+    #Removes sites from intAB present in other samples except for sample B and sample A
     for i in range(0,len(samples)):
-        if i != bSetIdx:
+        if i != bSetIdx and (samples[i].getFileName() != targetDict.getFileName()):
             for ele in samples[i].getKeys():
                 try:
                     intAB.remove(ele)
@@ -46,41 +49,58 @@ if __name__ == "__main__":
     GPN = vcfDict("../data/compareSNPs/tmp/GP2-3_N_m0.vcf")
     GPO = vcfDict("../data/compareSNPs/tmp/GP2-3_O_m0.vcf")
 
+    CCB = vcfDict("../data/compareSNPs/tmp/CC_B_m0.vcf")
+    CCC = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
+    CCD = vcfDict("../data/compareSNPs/tmp/CC_D_m0.vcf")
+    CCE = vcfDict("../data/compareSNPs/tmp/CC_E_m0.vcf")
+    CCF = vcfDict("../data/compareSNPs/tmp/CC_F_m0.vcf")
+    CCG = vcfDict("../data/compareSNPs/tmp/CC_G_m0.vcf")
+    CCH = vcfDict("../data/compareSNPs/tmp/CC_H_m0.vcf")
+    CCI = vcfDict("../data/compareSNPs/tmp/CC_I_m0.vcf")
+    CCJ = vcfDict("../data/compareSNPs/tmp/CC_J_m0.vcf")
+    CCK = vcfDict("../data/compareSNPs/tmp/CC_K_m0.vcf")
+    CCL = vcfDict("../data/compareSNPs/tmp/CC_L_m0.vcf")
+    CCM = vcfDict("../data/compareSNPs/tmp/CC_M_m0.vcf")
+    CCN = vcfDict("../data/compareSNPs/tmp/CC_N_m0.vcf")
+    CCO = vcfDict("../data/compareSNPs/tmp/CC_O_m0.vcf")
 
-    CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-    CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-    CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-    CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-CCC  = vcfDict("../data/compareSNPs/tmp/CC_C_m0.vcf")
-
-
-
-
-    samples = [GPB,GPC,GPD,GPE,GPF,GPG,GPH,GPI,GPJ,GPK,GPL,GPM,GPN,GPO]
-    for x in samples:
+    samples_CC = [CCB,CCC,CCD,CCE,CCF,CCG,CCH,CCI,CCJ,CCK,CCL,CCM,CCN,CCO]
+    samples_GP = [GPB,GPC,GPD,GPE,GPF,GPG,GPH,GPI,GPJ,GPK,GPL,GPM,GPN,GPO]
+    for x in samples_CC:
         x.loadDict() 
-    
+ 
     sample = sys.argv[1]
     sDict = vcfDict(sample)
     sDict.loadDict()
-    
+    sampleName = sDict.getFileName()
 
-    for i in range(0,len(samples)):
-        outFile = open(os.path.basename(sample) + ":" + samples[i].getFileName() + ".csv","w")
-        iSet = getExclInt(samples,i,set(sDict.getKeys()))
-        print "Sites CCM excl shares with %i: %i" %(i,len(getExclInt(samples,i,set(sDict.getKeys()))))
+    for i in range(0,len(samples_CC)):
+#        outFile = open(os.path.basename(sample) + ":" + samples_CC[i].getFileName() + ".csv","w")
+        iSet = getExclInt(samples_CC,i,sDict)
+        print "Sites %s excl shares with %s: %i" %(sampleName,samples_CC[i].getFileName(),len(iSet))
+#        writeColumns(outFile)
+#        for key in iSet:
+#            writeInfo(sDict.getLine(key),outFile)
+#        outFile.close()
+
+    for x in samples_GP:
+        x.loadDict()
+
+
+    for i in range(0,len(samples_GP)):
+        outFile = open(os.path.basename(sample) + ":" + samples_GP[i].getFileName() + ".csv","w")
+        iSet = getExclInt(samples_GP,i,sDict)
+        print "Sites %s excl shares with %s: %i" %(sampleName,samples_GP[i].getFileName(),len(iSet))
         writeColumns(outFile)
         for key in iSet:
             writeInfo(sDict.getLine(key),outFile)
         outFile.close()
+
+       
+
+
+
+
+
 
     sys.exit()
