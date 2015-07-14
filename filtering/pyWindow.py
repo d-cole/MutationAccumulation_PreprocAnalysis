d95aea792319cc49638a151ef1887cedb8fad9f8
d-cole <daniel.cole@mail.utoronto.ca>
2015-07-14 11:13:35 -0400

    reverted pyWindow with csvWriting options

--- filtering/pyWindow.py
+++ filtering/pyWindow.py
@@ -8,10 +8,9 @@ Analyzes vcf files from sites in a variable sized window
 """
 WINDOW_SIZE = 50
 
-def processWindow(inWindow):
+def processWindow(inWindow,outFile):
     """
     Given a list of the 50 sites in the window returns ...
-
 
     Percent of sites with depth below x?
     Average depth at sites?
@@ -31,56 +30,43 @@ def processWindow(inWindow):
 
         if site.isAltSite:
             alt_sites += 1
-
  
-    if float(DPtotal/50.0) < 200.00 or alt_sites > 5:
-       print "Alt site: " + str(alt_sites)
-       print "ref_sites: " + str(50 - alt_sites)
-
-       print inWindow[0].chrom + inWindow[0].pos +":" + inWindow[49].chrom +\
-            inWindow[49].pos
-       print str(DPtotal/50.0) 
-       print ""
-       
-    #print "% alt sites: " + str(alt_sites/50.0)
-    #print "Average depth in window: " + str(DPtotal/50.0)
-    #print "Ref reads #: " + str(site.getRefTotal())
-    #print "Alt reads #: " +  str(site.getAltTotal())
-    #
+    if True:
+        csvOutLine = ""
+        csvOutLine  = csvOutLine + inWindow[0].chrom +":"+ inWindow[0].pos + ","
+        csvOutLine = csvOutLine + inWindow[49].chrom +":" + inWindow[49].pos +","
+        csvOutLine = csvOutLine + str(alt_sites) +","
+        csvOutLine = csvOutLine + str(50 - alt_sites) + ","
+        csvOutLine = csvOutLine + str(DPtotal/50.0) + "\n"
+        outFile.write(csvOutLine)
 
 
 if __name__ == "__main__":
     vcf_loc = sys.argv[1]
     inWindow = []
-
+    outFile = open("CC_windowOut.csv","w")    
+    
+# CSV output file format 
+# start,        stop,       #Alt sites,      #Ref sites,     Average depth
+# pseudo0:1000, pseudo0:1050, 5,                45,             197
+#
+    outFile.write("start,stop,altCount,refCount,avgDepth" + "\n")
+    
     with open(vcf_loc) as f:
         for raw_line in f:
             if len(inWindow) == WINDOW_SIZE:
                 #Analyze site
-                processWindow(inWindow)
-#                print raw_line
-#                sys.exit()
-                #Output results
+                processWindow(inWindow,outFile)
                 inWindow = []
+            
 
             line = vcfLine(raw_line)
-    #        try:
-    #            line = vcfLine(raw_line) 
-    #        except:
-    #            e = sys.exc_info()[0]
-    #            print e
-    #            print traceback.format_exc()
-    #            print raw_line + "\n"
-    #
             if line.isDataLine:
                 #Only add dataLines to the window
                 inWindow.append(line) 
                     
 
-
-
-
-
+    outFile.close()
 
 
 
