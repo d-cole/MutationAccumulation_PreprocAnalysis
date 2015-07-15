932a427118d7108992673d34d88211fc83fb86bb
Daniel <daniel.cole@mail.utoronto.ca>
2015-07-15 13:14:56 -0400

    fixed

--- r/bpWindow.r
+++ r/bpWindow.r
@@ -1,29 +1,28 @@
+library(ggplot2)
+
 args <- commandArgs(TRUE)
 if(length(args) < 2) {
-if(length(args) < 1) {
   cat("Please specify location of .csv file \n")
 }
 
 #Read in csv file
 CC_window<-read.csv(args[1],stringsAsFactors=FALSE,na.string=".")
 #GP_window<-read.csv(args[2],stringsAsFactors=FALSE,na.string=".")
-CC_window<-transform(CC_window,altCount<-as.numeric(altCount))
+CC_window<-transform(CC_window,altCount<-as.numeric(altCount),refCount<-as.numeric(refCount))
 print(head(CC_window))
-#print(head(GP_window))
 
-#print(CC_window$altCount)
-#print(CC_window[CC_window$altCount != 0,])
+CC_window<-transform(CC_window,altRatio<-as.numeric(altCount/refCount))
+
+CC_window$altRatio <- CC_window$altCount/CC_window$refCount
+
+CC_w<-CC_window[CC_window$altCount != 0,]
+print(head(CC_w))
 
 jpeg("CC_window.jpg")
-plot(hist(CC_window$altCount,prob=F,breaks="FD",
-   main=paste("CC alt reads")))
+#plot(hist(CC_w$altRatio,breaks="FD",
+#   main=paste("CC alt reads")))
+ggplot(data=CC_w,aes(CC_w$altRatio)) + geom_histogram(binwidth=0.01) + xlim(c(0,1))
 dev.off()
-
-
-#jpeg("GP_window.jpg")
-#plot(hist(GP_window$altCount,prob=F,breaks="FD",
-#    main=paste("GP alt reads")))
-#dev.off()
 
 
 
