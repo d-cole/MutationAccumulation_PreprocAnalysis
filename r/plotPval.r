06719a6bbbe00854fe275351a1d6be5877b8bf24
d-cole <daniel.cole@mail.utoronto.ca>
2015-08-04 16:47:09 -0400

    modified plotPval and added Pvals with CC_rf6 with GQ

--- r/plotPval.r
+++ r/plotPval.r
@@ -22,17 +22,18 @@ readVals$altRef<-readVals$AD_alt/readVals$AD_ref
 #    plot <- ggplot() + aes(sample_data) + geom_histogram()
 #    ggsave(plot,file=paste(name,s,".jpg",sep=""))
 #}
-print(head(readVals))
-plot_alt_ref <- ggplot() + aes(readVals[readVals$AD_alt > 7,6]) + geom_histogram(binwidth = 0.1) + coord_cartesian(xlim=c(0,5))
-ggsave(plot_alt_ref,file=paste(name,"_alt_ref.jpg",sep=""))
+#print(head(readVals))
+#plot_alt_ref <- ggplot() + aes(readVals[readVals$AD_alt > 7,6]) + geom_histogram(binwidth = 0.1) + coord_cartesian(xlim=c(0,5))
+#ggsave(plot_alt_ref,file=paste(name,"_alt_ref.jpg",sep=""))
 
-plot_pVal <- ggplot() + aes(readVals[readVals$AD_alt > 5,5]) + geom_histogram(binwidth = 0.01)
-ggsave(plot_pVal,file=paste(name,"_pVal.jpg",sep=""))
+#plot_pVal <- ggplot() + aes(readVals[readVals$AD_alt > 5,5]) + geom_histogram(binwidth = 0.01)
+#ggsave(plot_pVal,file=paste(name,"_pVal.jpg",sep=""))
 
-plot_alt <- ggplot() + aes(readVals[readVals$AD_alt > 5,3]) + geom_histogram()
-ggsave(plot_alt,file=paste(name,"_alt.jpg",sep=""))
+##plot_alt <- ggplot() + aes(readVals[readVals$AD_alt > 5,3]) + geom_histogram()
+#ggsave(plot_alt,file=paste(name,"_alt.jpg",sep=""))
 
-
+plot <- ggplot(readVals,aes(GQ,AD_alt)) + geom_scatter()
+ggsave(plot,file=paste(name,"_GQ_Pval.jpg",sep=""))
 
 
 #    print(col)
